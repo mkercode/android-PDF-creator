e4c0b2f36a605f599301a019af218c34011bbf68
mkercode <mkercode@gmail.com>
2021-02-04 04:18:19 -0500

    added nav graph

--- app/src/main/java/com/loopbreakr/firstpdf/EditFragment.java
+++ app/src/main/java/com/loopbreakr/firstpdf/EditFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EditorFragment#newInstance} factory method to
+ * Use the {@link EditFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EditorFragment extends Fragment {
+public class EditFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class EditorFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public EditorFragment() {
+    public EditFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class EditorFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EditorFragment.
+     * @return A new instance of fragment EditFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EditorFragment newInstance(String param1, String param2) {
-        EditorFragment fragment = new EditorFragment();
+    public static EditFragment newInstance(String param1, String param2) {
+        EditFragment fragment = new EditFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class EditorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editor, container, false);
+        return inflater.inflate(R.layout.fragment_edit, container, false);
     }
 }
